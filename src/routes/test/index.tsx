c9d988cfdc4d016195e2fa9c87b3af1542921922
tom <tom@tomdeMacBook-Pro.local>
2021-05-21 18:24:46 +0800

    更新test小玩意儿

--- src/routes/test/index.tsx
+++ src/routes/test/index.tsx
@@ -1,25 +1,88 @@
 import React, { PureComponent } from "react";
 import "./index.scss";
 import { Button } from "antd-mobile";
-import { qs_parse } from "src/utils";
+import { Bubble } from "@utils/index";
+// import { qs_parse } from "src/utils";
+// const ROSE = require("@/assets/rose2.jpeg").default;
+const LONG = require("@/assets/dragon.jpg").default;
 export default class Test extends PureComponent<any, any> {
+  private canvas: HTMLCanvasElement | undefined;
+  private ctx: CanvasRenderingContext2D | null | undefined;
+  private img: HTMLImageElement = new Image();
+  private divEle: HTMLDivElement | undefined;
   constructor(props: any) {
     super(props);
     this.state = {
       phone: "15727531836",
+      imgload: false,
     };
   }
   componentDidMount() {
-    qs_parse();
+    this.get_canvas();
   }
+  // 将图片绘制到canvas
+  get_canvas = () => {
+    this.img.src = LONG;
+    this.img.onload = () => {
+      if (!!this.canvas) {
+        this.canvas.width = this.img.width;
+        this.canvas.height = this.img.height;
+        this.ctx = this.canvas?.getContext("2d");
+        this.ctx?.drawImage(this.img, 0, 0);
+        this.get_canvas_data();
+      }
+    };
+  };
+  // 获取并裁剪画布的点阵信息
+  get_canvas_data = () => {
+    if (this.ctx) {
+      const imageData = this.ctx?.getImageData(
+        0,
+        0,
+        this.img.width,
+        this.img.height
+      ).data;
+      this.ctx.fillStyle = "#fff";
+      this.ctx.fillRect(0, 0, this.img.width, this.img.height);
+      const gap = 8;
+      for (let h = 0; h < this.img.height; h += gap) {
+        for (let w = 0; w < this.img.width; w += gap) {
+          const position = (this.img.width * h + w) * 4;
+          const r = imageData[position],
+            g = imageData[position + 1],
+            b = imageData[position + 2];
+
+          if (r + g + b == 0) {
+            this.ctx.fillStyle = "pink";
+            this.ctx.fillRect(w, h, 4, 4);
+          }
+        }
+      }
+      this.generate_contaner(imageData, gap);
+    }
+  };
+  generate_contaner = (imageData: any, gap: any) => {
+    const img = this.img;
+    const ele = this.divEle;
+    for (let h = 0; h < this.img.height; h += gap) {
+      for (let w = 0; w < this.img.width; w += gap) {
+        new Bubble({ w, h, img, ele, imageData });
+      }
+    }
+  };
   render() {
     const { phone } = this.state;
     return (
       <section className="register-container">
-        <div>
+        <div className="canvas">
           <span>{phone}</span>
           <Button>呵呵</Button>
         </div>
+        <canvas ref={(node: any) => (this.canvas = node)}></canvas>
+        <div
+          ref={(node: any) => (this.divEle = node)}
+          className="container"
+        ></div>
       </section>
     );
   }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 import cookie from "js-cookie";
+const BUBBLE = require("@/assets/bubble.png").default;
 export function qs_parse(url: string = location.search) {
   url = url.replace(/#.*/, "");
   url.slice(0, 1);
@@ -24,3 +25,36 @@ export const save_cookie = (key: string, val: string, exp: number) => {
     path: "",
   });
 };
+export class Bubble {
+  private h: any;
+  private w: any;
+  private imageData: any;
+  private img: any;
+  private ele: any;
+  constructor(obj: any) {
+    this.h = obj.h;
+    this.w = obj.w;
+    this.imageData = obj.imageData;
+    this.img = obj.img;
+    this.ele = obj.ele;
+    this.render();
+  }
+  render() {
+    const dragonScale = 2;
+    const position = (this.img.width * this.h + this.w) * 4;
+    const r = this.imageData[position],
+      g = this.imageData[position + 1],
+      b = this.imageData[position + 2];
+    if (r + g + b === 0) {
+      const bubble = document.createElement("img");
+      bubble.src = BUBBLE;
+      bubble.setAttribute("class", "bubble");
+      const bubbleSize = Math.random() * 10 + 20;
+      bubble.style.left = this.w * dragonScale - bubbleSize / 2 + "px";
+      bubble.style.top = this.h * dragonScale - bubbleSize / 2 + "px";
+      bubble.style.width = bubble.style.height = bubbleSize + "px";
+      bubble.style.animationDuration = Math.random() * 6 + 4 + "s";
+      this.ele?.appendChild(bubble);
+    }
+  }
+}
