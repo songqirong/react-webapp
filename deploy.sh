c4b2360b383c5890277ae76075299397a8ccfbf9
tom <tom@tomdeMacBook-Pro.local>
2021-07-19 17:55:12 +0800

    调整样式

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 set -e
+# 删除更改
+git checkout tsconfig.json
+
 
 # 推到远程
 git add .
@@ -7,5 +10,7 @@ git push
 
 # build
 npm run build 
+# 删除更改
+git checkout tsconfig.json
 # 压缩文件并删除源文件夹
 zip -rm build.zip build
