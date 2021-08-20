60be663039b10355ed5825bfc0b6765a50539d34
tom <tom@tomdeMacBook-Pro.local>
2021-08-20 19:00:05 +0800

    修复风格问题

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,4 @@
-const { resolve } = require("path");
+const { resolve } = require('path');
 const {
   override,
   fixBabelImports,
@@ -6,41 +6,37 @@ const {
   overrideDevServer,
   addPostcssPlugins,
   addWebpackAlias,
-} = require("customize-cra");
-const addCustomize = () => (config) => {
-  return config;
-};
-const devServerConfig = () => (config) => {
-  return {
-    ...config,
-    // proxy: {
-    //   "/api": {
-    //     target: "http://localhost:8095",
-    //     changeOrigin: true,
-    //     wss: true,
-    //   },
-    // },
-  };
-};
+} = require('customize-cra');
+const addCustomize = () => (config) => config;
+const devServerConfig = () => (config) => ({
+  ...config,
+  // proxy: {
+  //   "/api": {
+  //     target: "http://localhost:8095",
+  //     changeOrigin: true,
+  //     wss: true,
+  //   },
+  // },
+});
 module.exports = {
   webpack: override(
     // 按需引入antd-nobile
-    fixBabelImports("import", {
-      libraryName: "antd-mobile",
-      style: "css",
+    fixBabelImports('import', {
+      libraryName: 'antd-mobile',
+      style: 'css',
     }),
     // 自动转换（px => rem）
-    addPostcssPlugins([require("postcss-px2rem")({ remUnit: 75 })]),
+    addPostcssPlugins([require('postcss-px2rem')({ remUnit: 75 })]),
     // 起别名
     addWebpackAlias({
-      ["@"]: resolve(__dirname, "src"),
-      ["src"]: resolve(__dirname, "src"),
-      ["@components"]: resolve(__dirname, "src/components"),
-      ["@redux"]: resolve(__dirname, "src/redux"),
-      ["@api"]: resolve(__dirname, "src/api"),
-      ["@utils"]: resolve(__dirname, "src/utils"),
+      ['@']: resolve(__dirname, 'src'),
+      ['src']: resolve(__dirname, 'src'),
+      ['@components']: resolve(__dirname, 'src/components'),
+      ['@redux']: resolve(__dirname, 'src/redux'),
+      ['@api']: resolve(__dirname, 'src/api'),
+      ['@utils']: resolve(__dirname, 'src/utils'),
     }),
-    addCustomize()
+    addCustomize(),
   ),
   devServer: overrideDevServer(devServerConfig(), watchAll()),
 };
